33e64bce0678c93f98b1809b69d012ee69e94625
kapanadze <akaki.kapanadze04@geolab.edu.ge>
2016-11-17 01:09:06 +0400

    Added project

---
+++ app/src/main/java/ge/faketorch/TorchActivity.java
@@ -0,0 +1,13 @@
+package ge.faketorch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TorchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_torch);
+    }
+}

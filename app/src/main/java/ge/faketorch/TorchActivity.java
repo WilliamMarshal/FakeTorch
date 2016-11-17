1ef4580b1d0bbb06456242ef7944631faa8bbc4d
kapanadze <akaki.kapanadze04@geolab.edu.ge>
2016-11-17 19:45:27 +0400

    few changes

--- app/src/main/java/ge/faketorch/TorchActivity.java
+++ app/src/main/java/ge/faketorch/TorchActivity.java
@@ -2,6 +2,8 @@ package ge.faketorch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TorchActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TorchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_torch);
+
+        Button onButton = (Button) findViewById(R.id.on_btn);
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

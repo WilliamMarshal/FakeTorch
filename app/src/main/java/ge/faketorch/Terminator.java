7e008b1a226d9a8b161d4c733a094c6e73e2aec6
kapanadze <akaki.kapanadze04@geolab.edu.ge>
2016-11-18 20:28:13 +0400

    დაემატა ტაიმერი

---
+++ app/src/main/java/ge/faketorch/Terminator.java
@@ -0,0 +1,18 @@
+package ge.faketorch;
+
+import android.os.CountDownTimer;
+
+/**
+ * Created by akaki on 18.11.16.
+ */
+
+public class Terminator {
+    public static void terminate(final CountDownTimer timer) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                timer.cancel();
+            }
+        }).start();
+    }
+}
--- app/src/main/java/ge/faketorch/TorchActivity.java
+++ app/src/main/java/ge/faketorch/TorchActivity.java
@@ -1,28 +1,37 @@
 package ge.faketorch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class TorchActivity extends AppCompatActivity {
+
+    private View mContainer;
+    private TextView mTimerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_torch);
 
-        Button onButton = (Button) findViewById(R.id.on_btn);
-        onButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.on_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewDissapear(view);
+                viewDisappear(view);
             }
         });
+
+        mContainer = findViewById(R.id.container);
+        mTimerLabel = (TextView) findViewById(R.id.timer_lbl);
     }
-    private void viewDissapear(final View view) {
+
+
+    private void viewDisappear(final View view) {
         Animation alphaAnim = new AlphaAnimation(1f, 0f);
         alphaAnim.setDuration(300);
 
@@ -31,7 +40,29 @@ public class TorchActivity extends AppCompatActivity {
             @Override
             public void run() {
                 view.setVisibility(View.GONE);
+                startTimer();
             }
         });
+    }
+
+    private void startTimer() {
+        new CountDownTimer(Integer.MAX_VALUE, 1000) {
+            int counter = 5;
+
+            @Override
+            public void onTick(long l) {
+                mTimerLabel.setText(counter + "");
+                if (counter == 0) {
+                    mTimerLabel.setVisibility(View.GONE);
+                    Terminator.terminate(this);
+                }
+                counter--;
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
     }
 }
